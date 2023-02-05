7b15340348292a87f3997ae3c88a8fbcb17d34a5
bsimmonds28 <bsimmonds28@gmail.com>
2023-02-05 15:06:06 -0500

    Completed project

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,124 @@
-// TODO: Include packages needed for this application
+//inquirer version 8.2.4 for collecting user input (In the command line, typed npm init and then typed npm i inquirer@8.2.4
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//fs for writing to the file system
+const fs = require('fs');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//Question prompts in the command line
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your project description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please enter information for installation.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please enter usage information.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please enter contribution guidelines.',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Please enter tests.',
+        name: 'tests',
+    },
+    //Choose a license for my application from a list of options
+    {
+        type: 'list',
+        message: 'Please enter usage information.',
+        name: 'license',
+        choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3'],
+    },
+  ])
+  .then((data) => {
+    //Create name of file
+    //const filename = `${data.name.toLowerCase().split(' ').join('')}.md`;
+    let licenseBadge;
+
+    //Select license badge
+    switch (data.license) {
+        case 'MIT':
+            licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+        case 'GPLv2':
+            licenseBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+            break;
+        case 'Apache':
+            licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+        case 'GPLv3':
+            licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            break;
+    };
+
+    //String template literals for generating a string version of the readme document before it is written to the file system
+    let readmeWrapper = `
+# ${data.name}
+
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} License.
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have additional questions, please email me at ${data.email}.
+To see more of my work, find me on GitHub at [${data.username}](https://github.com/${data.username})!
+`
+    //Function to create readme file and add code
+    fs.writeFile('README.md', readmeWrapper, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+
+});
 
 // TODO: Create a function to initialize app
 function init() {}
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# README Generator
+
+## Description
+ When creating an open source project on GitHub, it’s important to have a high-quality README for the app. I created a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.
+
+[![Watch the video](./Assets/readme-generator-screenshot.png)](https://youtu.be/YLsjh5MzSho)
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+In order to install inquirer, please use npm i inquirer@8.2.4.
+
+## Usage
+Through this project, you can quickly and easily generate a README file by using a command-line application to generate one. This allows the project creator to devote more time working on the project.
+
+## Contributing
+Thank you for your interest in my project. I am excited to see your contributions. Please follow the [Contributor Covenant](https://www.contributor-covenant.org/).
+
+## Questions
+If you have additional questions, please email me at bsimmonds28@gmail.com.
+To see more of my work, find me on GitHub at [bsimmonds28](https://github.com/bsimmonds28)!
